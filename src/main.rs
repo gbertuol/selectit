31bcd5d774130b76fe01c2d214cf7a3a072029b1
Guilherme Bertuol <bertuol@amazon.com>
2017-03-17 13:26:41 +0000

    Reading choices from stdin

---
+++ src/main.rs
@@ -0,0 +1,43 @@
+use std::io;
+use std::io::BufRead;
+
+fn main() {
+    let choices = read_choices();
+}
+
+fn read_choices() -> Vec<String> {
+    let stdin = io::stdin();
+    let mut lines = Vec::new();
+
+    let mut stdin = stdin.lock();
+    loop {
+        let mut input = String::new();
+        stdin.read_line(&mut input).unwrap();
+        trim_eol(&mut input);
+        if input.is_empty() { break; }
+        lines.push(input);
+    }
+
+    lines
+}
+
+fn trim_eol(string: &mut String) {
+    while let Some(x) = string.pop() {
+        if x != '\n' && x != '\r' {
+            string.push(x);
+            return;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{trim_eol};
+
+    #[test]
+    fn trim_eol_test() {
+        let mut x = "asdf\n".to_string();
+        trim_eol(&mut x);
+        assert_eq!("asdf", x);
+    }
+}
